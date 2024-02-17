de122f83b8c8d29dc5556f92e875fc66a13b599c
Oleh Toporovych <otoporovych2@gmail.com>
2024-02-17 20:25:33 +0100

    homework add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log("Form")
+const form = document.querySelector(".feedback-form");
+
+const keyForm = "feedback-form-state";
+
+
+function dataForm(form) {
+    const email = form.email.value.trim();
+    const message = form.message.value.trim();
+    return {
+        email,
+        message
+    }
+}
+
+form.addEventListener("input", (event) => {
+    event.preventDefault();
+    const data = dataForm(event.currentTarget);
+    const jsDate = JSON.stringify(data);
+    localStorage.setItem(keyForm, jsDate)
+})
+
+const item = localStorage.getItem(keyForm)
+
+if (item) {
+    const dateParse = JSON.parse(item)
+    form.email.value = dateParse.email;
+    form.message.value = dateParse.message;
+}
+
+
+form.addEventListener("submit", (event) => {
+    if (form.email.value === "" || form.message.value === "") {
+        alert("Будь ласка, заповніть обидва поля форми!");
+        return; 
+    }
+    event.preventDefault();
+    const lastDate = dataForm(form);
+    console.log(lastDate)
+
+    localStorage.removeItem(keyForm);
+    form.reset();
+})
